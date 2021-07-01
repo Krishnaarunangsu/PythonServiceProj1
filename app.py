7e52d8fa3f8a5cbb4313992305b1043bd8827ad5
Krishnaarunangsu <arunangsutech@gmail.com>
2021-07-01 23:02:02 +0530

    Request and Header

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 """
 Example python app with the Flask framework: http://flask.pocoo.org/
 """
-
+import json
 from os import environ
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, url_for
 from flask import render_template
 
 app = Flask(__name__)
@@ -54,6 +54,24 @@ def get_people():
     people = [{'name': 'Alice', 'birth-year': 1986},
               {'name': 'Bob', 'birth-year': 1985}]
     return jsonify(people)
+
+
+@app.route('/messages', methods = ['POST'])
+def api_message():
+
+    if request.headers['Content-Type'] == 'text/plain':
+        return "Text Message: " + request.data
+
+    elif request.headers['Content-Type'] == 'application/json':
+        return "JSON Message: " + json.dumps(request.json)
+
+    elif request.headers['Content-Type'] == 'application/octet-stream':
+        f = open('./binary', 'wb')
+        f.write(request.data)
+        f.close()
+        return "Binary message written!"
+    else:
+        return "415 Unsupported Media Type ;)"
 
 
 if __name__ == '__main__':
